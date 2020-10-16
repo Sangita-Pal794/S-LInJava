3b86313182512f98c3b1bc70715ec4de88e314c7
Sangita <pal07sangita@gmail.com>
2020-10-16 09:57:05 +0530

    Added UC1: Setting the starting position player at 0

---
+++ SnakeAndLadder.java
@@ -0,0 +1,10 @@
+class SnakeAndLadder {
+
+ // Declaring Variables
+  private static int player = 0;
+  private static int position = 0;
+  public static void main(String[] args) {
+   	System.out.println ("Welcome To Snake And Ladder!!");
+ }
+
+}
