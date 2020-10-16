9b9d6c2176b522a6734deac906c54f5f639c418a
Sangita <pal07sangita@gmail.com>
2020-10-16 11:06:57 +0530

    Added UC2:function added to decide the die roll

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 class SnakeAndLadder {
 
  // Declaring Variables
@@ -5,6 +7,8 @@ class SnakeAndLadder {
   private static int position = 0;
   public static void main(String[] args) {
    	System.out.println ("Welcome To Snake And Ladder!!");
+	Random r1 = new Random();
+    int diceRoll = r1.nextInt(6) + 1;
  }
 
 }
